03bf7d9bf9b5e0cee6a5eaa93ebf1ed1b742d9d8
Callum Coombes <ccoomzy@hotmail.co.uk>
2017-07-25 10:29:38 +0100

    Removed C++, Moved To Header
    Desc

--- ULog.h
+++ ULog.h
@@ -2,20 +2,15 @@
 
 #pragma once
 
-class DC_API ImplIT
+class /*DC_API*/ ImplIT
 {
 public:
 	ImplIT(){}
+	~ImplIT(){}
 
+	// You CAN edit/access this if you need too, but you shouldn't use Str() and the constructors
 	FString sString;
 	FString Str() { return sString; }
-
-	ImplIT operator= (const int& _iInt)
-	{
-		sString = ImplIT(_iInt).Str();
-
-		return ImplIT(_iInt);
-	}
 
 	// ImplIT(52) + MyString = 52MyString
 	ImplIT operator+ (const FString& _sString)
@@ -26,7 +21,7 @@ public:
 		return ImplIT(sLog);
 	}
 
-	// ImplIT(52) + ImplIT() = 52MyString
+	// ImplIT(52) + ImplIT("MyString") = 52MyString
 	ImplIT operator+ (const ImplIT& iIT)
 	{
 		FString sLog;
@@ -35,6 +30,16 @@ public:
 		return ImplIT(sLog);
 	}
 
+	// ImplIT(52) + ImplIT("MyString") = 52MyString
+	ImplIT operator* (const FString& _sString)
+	{
+		FString sLog;
+		sLog = sString + " " + _sString;
+
+		return ImplIT(sLog);
+	}
+
+	// ImplIT(52) * ImplIT("MyString") * 52MyString
 	ImplIT operator* (const ImplIT& iIT)
 	{
 		FString sLog;
@@ -43,37 +48,60 @@ public:
 		return ImplIT(sLog);
 	}
 
-	ImplIT(int MyInt)
-	{
-		sString = FString::FromInt(MyInt);
-	}
+	// ImplIT Constructors Used To Convert Values
 
-	ImplIT(float MyFloat)
-	{
-		sString = FString::SanitizeFloat(MyFloat);
-	}
-
-	ImplIT(FString _sString)
-	{
-		sString = _sString;
-	}
-
-	~ImplIT(){}
+	//ImplIT(bool MyBool) { sString = (MyBool ? "True" : "False"); }
+	//ImplIT(bool MyBool, FString sTrue, FString sFalse) { sString = (MyBool ? sTrue : sFalse); }
+	ImplIT(int MyInt) { sString = FString::FromInt(MyInt); }
+	ImplIT(float MyFloat) {	sString = FString::SanitizeFloat(MyFloat); }
+	ImplIT(FString _sString) { sString = _sString; }
 };
 
-static class DC_API ULog
+static class /*DC_API*/ ULog
 {
 public:
-	ULog();
+	ULog(){}
+	~ULog(){}
 
-	static void Log(FString sLog);
-	static void Log(ImplIT _ImplIT);
-	static void LogScreen(FString sLog);
-	static void LogScreen(ImplIT _ImplIT);
+// 	static void Log(FString sLog);
+// 	static void Log(ImplIT _ImplIT);
+// 	static void LogScreen(FString sLog);
+// 	static void LogScreen(ImplIT _ImplIT);
+// 	static void LogBoth(FString sLog);
+// 	static void LogBoth(ImplIT _ImplIT);
 
-	// Probably not needed anymore
-	//static void Log(ImplIT _ImplIT, ImplIT _ImplIT2);
-	//static void Log(ImplIT _ImplIT, ImplIT _ImplIT2, ImplIT _ImplIT3);
+	static void Log(FString sLog)
+	{
+		UE_LOG(LogClass, Log, TEXT("%s"), *sLog);
+	}
 
-	~ULog();
+	static void Log(ImplIT _ImplIT)
+	{
+		Log(_ImplIT.Str());
+	}
+
+	static void LogScreen(FString sLog)
+	{
+		if ( GEngine != nullptr )
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, sLog);
+		}
+	}
+
+	static void LogScreen(ImplIT _ImplIT)
+	{
+		LogScreen(_ImplIT.Str());
+	}
+
+	static void LogBoth(FString sLog)
+	{
+		Log(sLog);
+		LogScreen(sLog);
+	}
+
+	static void LogBoth(ImplIT _ImplIT)
+	{
+		Log(_ImplIT);
+		LogScreen(_ImplIT);
+	}
 };
